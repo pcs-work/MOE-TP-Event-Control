b62389c0d6253a5321cddc58d8d27717847be361
pcs-work <prashanth.springsystems@gmail.com>
2025-08-25 15:21:50 +0300

    Updates

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def get_prayer_times(lat: str, long: str) -> dict:
 
     LOGGER.info("----- SUCCESS -----")
     LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Retrieved Timings"))
+    LOGGER.info(f"Latitude={lat}, Longitude={long}")
 
     return {
         k: response.json()["data"]["timings"][k]
@@ -37,24 +38,27 @@ def get_prayer_times(lat: str, long: str) -> dict:
 
 
 def save_prayer_times(lat: str, long: str):
-    today = datetime.datetime.now().date().strftime("%d-%m-%Y")
+    # today = datetime.datetime.now().date().strftime("%d-%m-%Y")
 
-    url = f"https://api.aladhan.com/v1/timings/{today}?latitude={lat}&longitude={long}&method=4"
+    # url = f"https://api.aladhan.com/v1/timings/{today}?latitude={lat}&longitude={long}&method=4"
 
-    response = requests.get(url)
-    if response.status_code != 200:
-        return response.status_code, response.text
+    # response = requests.get(url)
+    # if response.status_code != 200:
+    #     return response.status_code, response.text
 
-    timings = {
-        k: response.json()["data"]["timings"][k]
-        for k in TIMES_NAMES
-        if k in response.json()["data"]["timings"]
-    }
+    # timings = {
+    #     k: response.json()["data"]["timings"][k]
+    #     for k in TIMES_NAMES
+    #     if k in response.json()["data"]["timings"]
+    # }
+
+    timings = get_prayer_times(lat, long)
 
     json.dump(timings, open("temp.json", "w"))
 
     LOGGER.info("----- SUCCESS -----")
     LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Saved Timings"))
+    LOGGER.info(f"Latitude={lat}, Longitude={long}")
     
 
 def check(server_ip: str):
@@ -65,9 +69,14 @@ def check(server_ip: str):
     timings = [t for t in timings.values()]
     timings = [datetime.datetime.strptime(t, "%H:%M").time() for t in timings]
 
+    LOGGER.info("----- CHECK -----")
+    LOGGER.info(datetime.datetime.now().strftime("Now - %H:%M"))
+
     for timing in timings:
         timing = datetime.datetime.combine(now.date(), timing)
-        trigger_dt = timing - datetime.timedelta(minutes=1)
+
+        # Activate the trigger 5 minutes before the prayer time
+        trigger_dt = timing - datetime.timedelta(minutes=5)
 
         if trigger_dt == current_time:
             url = f"https://{server_ip}/triplecare/eventHttpRequest.php?id=12&key=513e02950d9d0f4838eede664aa86df3&status=1"
@@ -83,15 +92,25 @@ def check(server_ip: str):
         
 
 def main():
-    args: str = "--operation"
+    args_1: tuple = ("--operation", "-op")
+    args_2: tuple = ("--server-ip", "-sip")
 
     operation: Union[str, None] = None
+    server_ip: str = "10.10.0.201"
 
-    if args in sys.argv:
-        operation = sys.argv[sys.argv.index(args) + 1]
+    if args_1[0] in sys.argv:
+        operation = sys.argv[sys.argv.index(args_1[0]) + 1]
+    if args_1[1] in sys.argv:
+        operation = sys.argv[sys.argv.index(args_1[1]) + 1]
+    
+    if args_2[0] in sys.argv:
+        server_ip = sys.argv[sys.argv.index(args_2[0]) + 1]
+    if args_2[1] in sys.argv:
+        server_ip = sys.argv[sys.argv.index(args_2[1]) + 1]
 
     if operation is None:
-        sys.exit("No Operation")
+        LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - No Operation Specified"))
+        sys.exit(f"{BREAKER}\nNo Operation Specified\n{BREAKER}")
 
     if operation == "save":
         save_prayer_times(lat="24.7136", long="46.6753")
@@ -100,7 +119,7 @@ def main():
         _ = get_prayer_times(lat="24.7136", long="46.6753")
 
     if operation == "check":
-        check("10.40.0.10")
+        check(server_ip)
 
 
 if __name__ == "__main__":
