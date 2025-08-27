e50a7c3ab231f8a188913bb6f35aac3445316336
pcs-work <prashanth.springsystems@gmail.com>
2025-08-27 10:30:32 +0300

    Updates

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ BREAKER: str = "\n" + 50 * "*" + "\n"
 TIMES_NAMES: tuple = ("Fajr", "Dhuhr", "Asr", "Maghrib", "Isha")
 LOCATION_INFO = {
     "riyadh" : ["24.1736", "46.6753", "id=12&key=513e02950d9d0f4838eede664aa86df3"],
-    "jeddah" : ["21.5292", "39.1611"],
+    "jeddah" : ["21.5292", "39.1611", "id=14&key=d486da6d64f8571a964d96365fe395cd"],
+    "dhahran" : ["26.2361", "50.0393", "id=15&key=7b1a65c9cb29b9d1eabbbb1d5e4e60fb"]
 }
 HEADERS: dict = {"Content-Type": "application/json"}
 
@@ -25,74 +26,77 @@ if not os.path.exists(SAVE_DIR):
 LOGGER = logging.getLogger("main.py")
 logging.basicConfig(filename=os.path.join(SAVE_DIR, "run.log"), encoding='utf-8', level=logging.DEBUG)
 
+class App:
+    @staticmethod
+    def get_prayer_times(location: str) -> dict:
+        today = datetime.datetime.now().date().strftime("%d-%m-%Y")
+        lat = LOCATION_INFO[location][0]
+        long = LOCATION_INFO[location][1]
 
-def get_prayer_times(location: str) -> dict:
-    today = datetime.datetime.now().date().strftime("%d-%m-%Y")
-    lat = LOCATION_INFO[location][0]
-    long = LOCATION_INFO[location][1]
+        url = f"https://api.aladhan.com/v1/timings/{today}?latitude={lat}&longitude={long}&method=4"
 
-    url = f"https://api.aladhan.com/v1/timings/{today}?latitude={lat}&longitude={long}&method=4"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return response.status_code, response.text
 
-    response = requests.get(url)
-    if response.status_code != 200:
-        return response.status_code, response.text
+        LOGGER.info("----- SUCCESS -----")
+        LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Retrieved Timings"))
+        LOGGER.info(f"Location: {location}")
 
-    LOGGER.info("----- SUCCESS -----")
-    LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Retrieved Timings"))
-    LOGGER.info(f"Location: {location}")
+        return {
+            k: response.json()["data"]["timings"][k]
+            for k in TIMES_NAMES
+            if k in response.json()["data"]["timings"]
+        }
 
-    return {
-        k: response.json()["data"]["timings"][k]
-        for k in TIMES_NAMES
-        if k in response.json()["data"]["timings"]
-    }
+    @staticmethod
+    def save_prayer_times(location: str) -> None:
+        timings = App.get_prayer_times(location)
 
+        json.dump(timings, open(os.path.join(SAVE_DIR, f"{location}.json"), "w"))
 
-def save_prayer_times(location: str) -> None:
-    timings = get_prayer_times(location)
-
-    json.dump(timings, open(os.path.join(SAVE_DIR, f"{location}.json"), "w"))
-
-    LOGGER.info("----- SUCCESS -----")
-    LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Saved Timings"))
-    LOGGER.info(f"Location: {location}")
-
+        LOGGER.info("----- SUCCESS -----")
+        LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Saved Timings"))
+        LOGGER.info(f"Location: {location}")
     
-def check(server_ip: str, location: str):
-    now = datetime.datetime.now()
-    current_time = now.replace(second=0, microsecond=0)
+    @staticmethod
+    def activate(server_ip, location: str):
+        url = f"https://{server_ip}/triplecare/eventHttpRequest.php?{LOCATION_INFO[location][2]}&status=1"
+        try:
+            response = requests.get(url, verify=False, timeout=1)
+            if response.status_code == 200:
+                LOGGER.info("----- SUCCESS -----")
+                LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Event Activated"))
+                LOGGER.info(f"Location: {location}")
+        except Exception as e:
+            LOGGER.debug("----- FAILURE -----")
+            LOGGER.debug(e)
 
-    timings = json.load(open(os.path.join(SAVE_DIR, f"{location}.json"), "r"))
-    timings = [t for t in timings.values()]
-    timings = [datetime.datetime.strptime(t, "%H:%M").time() for t in timings]
+    @staticmethod
+    def check(server_ip: str, location: str):
+        now = datetime.datetime.now()
+        current_time = now.replace(second=0, microsecond=0)
 
-    LOGGER.info("----- CHECK -----")
-    LOGGER.info(datetime.datetime.now().strftime("Now - %H:%M"))
+        timings = json.load(open(os.path.join(SAVE_DIR, f"{location}.json"), "r"))
+        timings = [t for t in timings.values()]
+        timings = [datetime.datetime.strptime(t, "%H:%M").time() for t in timings]
 
-    for timing in timings:
-        timing = datetime.datetime.combine(now.date(), timing)
+        LOGGER.info("----- CHECK -----")
+        LOGGER.info(datetime.datetime.now().strftime("Now - %H:%M"))
+        LOGGER.info(f"Location: {location}")
 
-        if timing < now:
-            continue
+        for timing in timings:
+            timing = datetime.datetime.combine(now.date(), timing)
 
-        # Activate the trigger 5 minutes before the prayer time
-        trigger_dt = timing - datetime.timedelta(minutes=5)
+            if timing < now:
+                continue
 
-        if trigger_dt == current_time:
-            activate(server_ip, location)
+            # Activate the trigger 1 minute before the prayer time
+            trigger_dt = timing - datetime.timedelta(minutes=1)
 
+            if trigger_dt == current_time:
+                App.activate(server_ip, location)
 
-def activate(server_ip, location: str):
-    url = f"https://{server_ip}/triplecare/eventHttpRequest.php?{LOCATION_INFO[location][2]}&status=1"
-    try:
-        response = requests.get(url, verify=False, timeout=1)
-        if response.status_code == 200:
-            LOGGER.info("----- SUCCESS -----")
-            LOGGER.info(datetime.datetime.now().strftime("%d-%m-%Y - %H:%M:%S - Event Activated"))
-    except Exception as e:
-        LOGGER.debug("----- FAILURE -----")
-        LOGGER.debug(e)
-        
 
 def main():
     args_1: tuple = ("--operation", "-op")
@@ -129,17 +133,17 @@ def main():
             sys.exit(f"{BREAKER}\nNo Operation Specified\n{BREAKER}")
         
         if operation == "save":
-            save_prayer_times(location)
+            App.save_prayer_times(location)
         
         if operation == "get":
-            print(get_prayer_times(location))
+            print(App.get_prayer_times(location))
 
         if operation == "check":
-            check(server_ip, location)
+            App.check(server_ip, location)
 
     else: 
-        save_prayer_times(location)
-        activate(server_ip, location)
+        App.save_prayer_times(location)
+        App.activate(server_ip, location)
 
 
 if __name__ == "__main__":
